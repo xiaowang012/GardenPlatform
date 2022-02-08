a7b2538217a6ab971183c938a304a25f650c8897
xiaowang012 <1300202481@qq.com>
2022-02-08 09:50:13 +0800

    更新视图

--- app.py
+++ app.py
@@ -24,9 +24,80 @@ db.init_app(app)
 #创建数据表
 #db.create_all(app=app)
 
-@app.route("/")
-def func():
-    form = {'1':2}
+#跳转到主页或登录页
+@app.route('/',methods = ['POST','GET'])
+def index():
+    if 'user_id' in session:
+       return redirect('home')
+    else:
+        return redirect('login')
+
+#用户注册
+@app.route('/register',methods = ['POST','GET'])
+def register():
+    form = RegisterForms()
+    if request.method == 'GET':
+        return render_template('register.html',form = form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            user=request.form['username']
+            passw=request.form['password'] 
+            if not User.query.filter_by(username = user).first():
+                try:
+                    username = user
+                    salt = str(time.time())
+                    hash_pwd = get_hash_value(passw,salt)
+                    add_time = time.strftime('%Y-%m-%d %H:%M:%S')
+                    group_id = 2
+                    data = User(username,hash_pwd,salt,group_id,add_time)
+                    db.session.add(data)
+                    db.session.commit()
+                    message = 'Sign up: '+ user + ' SUCCESS!'
+                    dic2 = {'title':'SUCCESS!','message':message,'frame_type':'alert alert-success alert-dismissable'}
+                    return render_template('register.html',form = form,dic2 = dic2)
+                except:
+                    db.session.rollback()
+                    message = 'Sign up: '+ user + ' ERROR!'
+                    dic2 = {'title':'ERROR!','message':message,'frame_type':'alert alert-dismissable alert-danger'}
+                    return render_template('register.html',form = form,dic2 = dic2)
+                finally:
+                    db.session.close()
+                
+            else:
+                dic1 = {'title':'fail','message':'The user name already exists, please do not re-register!'}
+                return render_template('register.html',form = form,dic1 = dic1)
+        else:
+            return render_template('register.html',form = form) 
+
+#用户登录
+@app.route('/login',methods = ['POST','GET'])
+def login():
+    # if 'user_id' in session:
+    #    return redirect('home')
+    form = UserForms()
+    if request.method == 'GET':
+        return render_template('index.html',form = form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            user=request.form['username']
+            passw=request.form['password'] 
+            res = User.query.filter(User.username == user).first()
+            if res:
+                #验证登录密码的哈希值是否和数据库中的密码哈希值相等
+                new_pwd = get_hash_value(passw,res.salt)
+                if new_pwd == res.hash_pwd:
+                    #验证通过
+                    session['user_id'] = user
+                    return 'Login OK!'
+                    #return redirect(url_for('home'))
+                else:
+                    dic1 = {'title':'error','message':'Incorrect password or user does not exist!'}
+                    return render_template('index.html',form =form,dic1 =dic1)
+            else:
+                dic1 = {'title':'error','message':'Incorrect password or user does not exist!'}
+                return render_template('index.html',form =form,dic1 =dic1)
+        else:
+            return render_template('index.html',form = form)
     return render_template('index.html',form = form)
 
 
